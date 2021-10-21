ce2221ef03e0aed7b8220bc96e329e7526011ef2
Dimitriy Marchenko <dmitriy.marchenko@lightspeedvt.com>
2021-10-21 17:30:34 +0200

    same as before

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,7 @@ import "./scss/styles.scss";
 import "./css/colors.css";
 import "./css/custom.css";
 import "./css/text-content.css";
-import SlideNavigation from "./ts/slide-navigation";
 import Widget from "./ts/widget";
 
 // Init the Widget
-const widget = new Widget("lslf__login-form");
-
-// Init Navigation
-new SlideNavigation(widget.container);
-
-// Init AJAX form
-/* 
-    Here we intercept login form submit
-    Throw error if not HTTPS 
-    Post login password
-    If answer is 401 display .error
-    Otherwise redirect to "response text"
-    if "response text" is not URL, 
-    remove backdrop and close itself
-*/
+const widget = new Widget("lslf__login-widget");
