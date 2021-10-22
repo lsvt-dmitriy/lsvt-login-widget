f4629ebf10b6e2c668f3fae8d10743f153d2cbb4
Dimitriy Marchenko <dmitriy.marchenko@lightspeedvt.com>
2021-10-22 11:35:13 +0200

    add text replaceables (broke sliding labels)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "./css/cleanslate.css";
 import "./scss/styles.scss";
 import "./css/colors.css";
+import "./css/custom.css";
+import "./css/text-content.css";
 import Widget from "./ts/widget";
 
 // Init the Widget
